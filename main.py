692490842126e6bdf8d1a009f95b1322a5eb5ded
林哲彦 <linzheyan@linzheyandeMBP.home>
2025-02-12 21:45:28 +0100

    update codes to test policy iteration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ZombieEscapeEnv import ZombieEscapeEnv
-
+from policy_iteration import policy_iteration
 # Create and test the environment
-env = ZombieEscapeEnv(render_mode='human')
+env = ZombieEscapeEnv(render_mode='human', fixed_seed = 35)
 state, info = env.reset()
 """
 # Reset environment and render
@@ -12,6 +12,7 @@ env.step(2)
 
 env.render()
 """
+'''
 terminal = False
 while not terminal:
     action = env.action_space.sample()
@@ -21,3 +22,16 @@ while not terminal:
 env.close()
 #episode = env.sample_episode()
 # print("Collected Episode:", episode)
+
+'''
+V,policy = policy_iteration(env, 0.93, 0.0000000001)
+#print(policy)
+
+terminal = False
+while not terminal:
+    action = policy[env.s]
+    next_state, reward, terminal = env.step(action)[:3]
+    env.render()
+    state = next_state
+env.close()
+
