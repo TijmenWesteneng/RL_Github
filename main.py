8dc318f1c2f77972eab8f7d7e929c989eda443b3
Nicolas Forcella <nicolas.forcella@ru.nl>
2025-02-17 08:43:07 -0300

    "Add value iteration and auxiliary functions"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ZombieEscapeEnv import ZombieEscapeEnv
 from policy_iteration import policy_iteration
+from value_iteration import ValueIteration
 # Create and test the environment
 env = ZombieEscapeEnv(render_mode='human', fixed_seed = 35)
 state, info = env.reset()
@@ -24,12 +25,15 @@ env.close()
 # print("Collected Episode:", episode)
 
 '''
-V,policy = policy_iteration(env, 0.93, 0.0000000001)
+#V,policy = policy_iteration(env, 0.93, 0.0000000001)
 #print(policy)
+new_value, new_policy = ValueIteration(env, 0.94, 0.01).get_training_results()
+print(new_policy)
 
 terminal = False
 while not terminal:
-    action = policy[env.s]
+    action = new_policy[env.s]
+    print(action)
     next_state, reward, terminal = env.step(action)[:3]
     env.render()
     state = next_state
---
+++ value_iteration.py
@@ -0,0 +1,79 @@
+from ZombieEscapeEnv import ZombieEscapeEnv
+import numpy as np
+
+class ValueIteration:
+    def __init__(self, zombie_environment:ZombieEscapeEnv, gamma, theta):
+        
+        #INITIALIZE CONFIG PARAMETERS
+        self.gamma = gamma
+        self.theta = theta
+        #INITIALIZE ENVIRONMENT VALUES
+        self.zombie_environment = zombie_environment
+        self.number_of_actions = zombie_environment.action_space.n
+        self.number_of_states = zombie_environment.observation_space.n
+        #INITIALIZE POLICY PROPERTY
+        self.policy = np.zeros(self.number_of_states)
+        #INITIALIZE VALUE FUNCTION
+        self.initialize_value_function()
+        #INITIALIZE CLASS VAR
+        self.trained = False
+
+    def initialize_value_function(self):
+        self.value_function = np.zeros(self.number_of_states)
+        for state in range(self.number_of_states):
+            if self.zombie_environment.is_terminal(state):
+                self.value_function[state] = self.zombie_environment.get_state_reward(state)
+        
+    
+    def single_value_iteration(self):
+        #Initialize q values function to store the values for computing max
+        new_value_function = np.zeros(self.number_of_states)
+        delta = 0
+        for state in range(self.number_of_states):
+            #Avoid updating the values of terminal states
+            if self.zombie_environment.is_terminal(state):
+                self.value_function[state] = self.zombie_environment.get_state_reward(state)
+                continue
+            
+            values = np.zeros(self.number_of_actions) #Store the values of the different actions
+            for action in range(self.number_of_actions):
+                value = 0
+                # prob is a tuple of (transition probability, next state, reward)
+                for prob in self.zombie_environment.P[state][action]:
+                    value += prob[0]*(prob[2] + self.gamma*self.value_function[prob[1]]) #The value for an action
+                
+                values[action] = value
+            #Get max value, best action and store the value and best action
+            max_value = np.max(values, axis=0)
+            best_action = np.argmax(values)
+            self.policy[state] = best_action
+            new_value_function[state] = max_value
+            #update delta
+            delta = max(delta, abs(self.value_function[state] - new_value_function[state]))
+
+        #update policy
+        self.value_function = new_value_function
+        
+        return delta
+                
+                
+    def run_training(self):
+        #Run value iteration until convergence
+        delta = self.single_value_iteration()
+        while delta > self.gamma:
+            delta = self.single_value_iteration()
+
+        #Given states find optimal policy
+        self.trained = True
+
+    def get_training_results(self):
+        #If model has not been trained
+        if not self.trained:
+            self.run_training()
+        
+        return self.value_function, self.policy
+
+
+        
+        
+
