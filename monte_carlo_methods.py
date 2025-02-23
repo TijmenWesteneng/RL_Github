96b8324c76e811b13b3bf9bf38c0cb489267e37a
Paul Bonnie <paulbonnie54@gmail.com>
2025-02-23 18:42:48 +0100

    added calculate_expected_return function in monte_carlo_methods.py

--- monte_carlo_methods.py
+++ monte_carlo_methods.py
@@ -12,7 +12,7 @@ class MonteCarloLearning(LearningAlgorithm):
         self.state_reward = None #Sum of rewards collected for the state action pair, np array [state, action]
         self.count_state_visits = None #Count of visits for the state action pair, np array [state, action]
 
-    def calculate_expected_return(self, episode, mode):
+    def calculate_expected_return(self, episode, mode, gamma):
         """
         Calculate first-visit G_t recursively and update the count of visited + returns 
         
@@ -20,7 +20,26 @@ class MonteCarloLearning(LearningAlgorithm):
         episode: a list of tuples containing the rewards.
         mode: state or state-action
         """
-        pass
+    
+        for t in range(len(episode) -1, -1, -1): # Loop from last index to first index of the episode
+            state, action, reward = episode[t]
+
+            G = gamma * G + reward
+
+            if state not in [x[0] for x in episode[:t]]:
+                
+                if mode == "state_value":
+                    self.state_reward[state] += G
+                    self.count_state_visits[state] += 1
+                    
+                elif mode == "state_action_value":
+                    self.state_action_reward[state, action] += G
+                    self.count_state_action_visits[state, action] += 1
+
+                    
+
+
+
 
 
 
