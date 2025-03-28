e5c48288ef9c6cdf6b8c596fc096694504c72602
TijmenWesteneng <t.r.m.westeneng@gmail.com>
2025-03-28 17:18:52 +0100

    Added cumulative rewards comparison to comparison.py I thought I already commited this, but maybe something went wrong

--- comparison.py
+++ comparison.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 from typing import List
+import numpy as np
 
 from ZombieEscapeEnv import ZombieEscapeEnv
 from learning_algorithm import LearningAlgorithm
 from value_iteration import ValueIteration
 from td_q_learning import TDQLearning
+from monte_carlo_control import MonteCarloControl
 from SARSA import SARSA
 
-def compare_algs(alg_list: List[LearningAlgorithm]):
+def compare_algs_error(alg_list: List[LearningAlgorithm]):
     # Check if all algs have same amount of episodes
     episode_amount = alg_list[0].episodes
     for alg in alg_list[1::]:
@@ -21,23 +23,53 @@ def compare_algs(alg_list: List[LearningAlgorithm]):
     # Plot errors for all algs
     index = range(len(alg_list[0].errors))
     for alg in alg_list:
-        plt.plot(index, alg.errors, label=f"{type(alg).__name__}")
+        plt.plot(index, alg.errors, label=alg)
 
     # Labels and visualisation
     plt.xlabel('Episodes')
     plt.ylabel('Root Mean Squared Error')
-    plt.title("RMSE values in Q-Learning for different alphas")
+    plt.title("RMSE values over episodes for different algorithms")
     plt.legend(title="Algorithms")
     plt.show()
 
-# Create the environment we'll use to train
-env = ZombieEscapeEnv(render_mode=None, fixed_seed=71, gamma=0.93)
+def compare_algs_cum(alg_list: List[LearningAlgorithm], training_amount = 2):
+    average_cum_reward_list = []
+    for j in range(len(alg_list)):
+        average_cum_reward_list.append([])
 
-# Running policy iteration as a comparison baseline
-value_iteration = ValueIteration(env, 0.00001)
-vi_v, vi_policy = value_iteration.get_training_results()
+    for i in range(training_amount):
+        for j, alg in enumerate(alg_list):
+            print(f"Now training: {alg} for iteration {i+1}")
+            alg.get_training_results()
+            average_cum_reward_list[j].append(alg.cum_reward_list)
+            alg.trained = False
 
-td_q_learning = TDQLearning(zombie_environment=env, target_values=vi_v, alpha=0.005)
-td_q_learning_2 = TDQLearning(zombie_environment=env, target_values=vi_v, alpha=0.01)
+    matrix = []
+    for i in range(len(alg_list)):
+        matrix.append(np.zeros((len(average_cum_reward_list[i]), len(average_cum_reward_list[i][0]))))
 
-compare_algs([td_q_learning, td_q_learning_2])
+        for j in range(len(average_cum_reward_list)):
+            index, values = zip(*average_cum_reward_list[i][j])
+            matrix[i][j] = np.asarray(values, dtype=np.float32)
+
+        average_array = np.array(matrix[i]).mean(axis=0)
+        plt.scatter(index, average_array, label=alg_list[i])
+    plt.title(f"Cumulative reward over {training_amount} training iterations")
+    plt.xlabel("Episode")
+    plt.ylabel("Cumulative reward")
+    plt.legend(title="Algorithms")
+    plt.show()
+
+if __name__ == "__main__":
+    # Create the environment we'll use to train
+    env = ZombieEscapeEnv(render_mode=None, fixed_seed=71, gamma=0.93)
+
+    # Running policy iteration as a comparison baseline
+    value_iteration = ValueIteration(env, 0.00001)
+    vi_v, vi_policy = value_iteration.get_training_results()
+
+    td_q_learning = TDQLearning(zombie_environment=env, episodes=100000, target_values=vi_v, alpha=0.01)
+    monte_carlo = MonteCarloControl(zombie_environment=env, episodes=100000, target_values=vi_v)
+
+    #compare_algs_error([td_q_learning, monte_carlo])
+    compare_algs_cum([td_q_learning, monte_carlo])
--- td_q_learning.py
+++ td_q_learning.py
@@ -24,8 +24,8 @@ class TDQLearning(LearningAlgorithm):
         self.qsa = np.zeros((self.zombie_environment.observation_space.n, self.zombie_environment.action_space.n))
         self.policy = np.zeros(self.zombie_environment.observation_space.n)
 
-        self.target_values = target_values
-        self.errors = np.zeros(self.episodes)
+    def __repr__(self):
+        return f"{type(self).__name__}(alpha={self.alpha})"
 
     @staticmethod
     def epsilon_policy(epsilon, Q_S_A, state, env):
@@ -59,7 +59,7 @@ class TDQLearning(LearningAlgorithm):
                 state = next_state
 
             # Every x episodes calculate the cumulative reward of the current policy
-            if episode % 100 == 0:
+            if episode % round(self.episodes / 1000) == 0:
                 self.policy = np.argmax(self.qsa, axis=1)
                 self.calc_policy_reward(episode_n = episode)
 
@@ -75,46 +75,84 @@ class TDQLearning(LearningAlgorithm):
         self.policy = np.argmax(self.qsa, axis=1)
         self.value_function = np.max(self.qsa, axis=1)
 
-# Create the environment we'll use to train
-env = ZombieEscapeEnv(render_mode='ansi', fixed_seed=24, gamma=0.93)
+if __name__ == "__main__":
+    # Create the environment we'll use to train
+    env = ZombieEscapeEnv(render_mode=None, fixed_seed=71, gamma=0.93)
 
-# Running policy iteration as a comparison baseline
-value_iteration = ValueIteration(env, 0.00001)
-V, policy = value_iteration.get_training_results()
+    # Running policy iteration as a comparison baseline
+    value_iteration = ValueIteration(env, 0.00001)
+    vi_v, vi_policy = value_iteration.get_training_results()
 
-# Create instance of the learning algorithm and use it to train and display the resulting policy and results
-td_q_learning = TDQLearning(zombie_environment=env, target_values=V)
+    """
+    # Create instance of the learning algorithm and use it to train and display the resulting policy and results
+    td_q_learning = TDQLearning(zombie_environment=env, target_values=vi_v, alpha=0.01)
+    
+    tdq_v, tdq_policy = td_q_learning.get_training_results()
+    td_q_learning.plot_cum_reward()
+    td_q_learning.visualise_policy()
+    td_q_learning.visualise_values_heatmap()
+    td_q_learning.visualise_values_difference(abs=True, heatmap=True)
+    td_q_learning.plot_error()
+    print(td_q_learning.errors[-1])
+    """
 
-td_q_learning.get_training_results()
-td_q_learning.plot_cum_reward()
-td_q_learning.visualise_policy()
-td_q_learning.visualise_values_heatmap()
-td_q_learning.plot_error()
-print(td_q_learning.qsa)
 
-"""
-# The part below is for averaging the cumulative reward over multiple trainings and save it in a file to plot later
-average_cum_reward_list = []
+    td_q_learning_1 = TDQLearning(zombie_environment=env, target_values=vi_v, alpha=0.01)
+    td_q_learning_1.get_training_results()
 
-for i in tqdm(range(10)):
-    td_q_learning = TDQLearning(zombie_environment=env)
-    td_q_learning.get_training_results()
-    average_cum_reward_list.append(td_q_learning.cum_reward_list)
+    td_q_learning_2 = TDQLearning(zombie_environment=env, target_values=vi_v, alpha=0.005)
+    td_q_learning_2.get_training_results()
 
-with open('avg_cum_reward_list.txt', 'w+') as f:
-    f.write(str(average_cum_reward_list))
+    td_q_learning_3 = TDQLearning(zombie_environment=env, target_values=vi_v, alpha=0.0025)
+    td_q_learning_3.get_training_results()
 
-f.close()
-"""
+    index = range(len(td_q_learning_1.errors))
+    plt.plot(index, td_q_learning_1.errors, label="0.01")
+    plt.plot(index, td_q_learning_2.errors, label="0.005")
+    plt.plot(index, td_q_learning_3.errors, label="0.0025")
+    plt.xlabel('Episodes')
+    plt.ylabel('Root Mean Squared Error')
+    plt.title("RMSE values in Q-Learning for different alphas")
+    plt.legend(title="Alpha")
+    plt.show()
 
-# From here on we run the model and display it on the screen, so this is not for training
-test_env = ZombieEscapeEnv(render_mode='human', fixed_seed=24)
-state = test_env.reset()
+    """
+    # The part below is for averaging the cumulative reward over multiple trainings and save it in a file to plot later
+    average_cum_reward_list = []
+    TRAINING_AMOUNT = 10
+    
+    for i in tqdm(range(10)):
+        td_q_learning = TDQLearning(zombie_environment=env, target_values=vi_v)
+        td_q_learning.get_training_results()
+        average_cum_reward_list.append(td_q_learning.cum_reward_list)
+    
+    matrix = np.zeros((len(average_cum_reward_list), len(average_cum_reward_list[0])))
+    
+    for i in range(len(average_cum_reward_list)):
+        index, values = zip(*average_cum_reward_list[i])
+        matrix[i] = np.asarray(values, dtype=np.float32)
+    
+    average_array = np.array(matrix).mean(axis=0)
+    plt.plot(index, average_array)
+    plt.title(f"Cumulative reward over {TRAINING_AMOUNT} training iterations")
+    plt.xlabel("Episode")
+    plt.ylabel("Cumulative reward")
+    plt.show()
+    
+    # with open('avg_cum_reward_list.txt', 'w+') as f:
+    #    f.write(str(average_cum_reward_list))
+    #
+    # f.close()
+    """
 
-terminal = False
-while not terminal:
-    action = td_q_learning.policy[state]
-    next_state, reward, terminal = test_env.step(action)[:3]
-    test_env.render()
-    state = next_state
+    # From here on we run the model and display it on the screen, so this is not for training
+    test_env = ZombieEscapeEnv(render_mode='human', fixed_seed=71)
+    state = test_env.reset()
+
+    terminal = False
+    while not terminal:
+        action = tdq_policy[state]
+        next_state, reward, terminal = test_env.step(action)[:3]
+        test_env.render()
+        state = next_state
 
